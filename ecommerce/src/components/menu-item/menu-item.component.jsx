6b2c27b782bb0e11ff23a0d34f84d384a370ba6e
unknown <zoka_cs_pro@hotmail.com>
2020-09-12 13:07:41 +0200

    Added google font 'open sans condensed', styled the menu item content

--- ecommerce/src/components/menu-item/menu-item.component.jsx
+++ ecommerce/src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl }) => (
+const MenuItem = ({ title, imageUrl, size }) => (
   <div
     style={{
       backgroundImage: `url(${imageUrl})`,
     }}
-    className='menu-item'
+    className={`${size} menu-item`}
   >
     <div className='content'>
-      <h1 className='title'>{title}</h1>
+      <h1 className='title'>{title.toUpperCase()}</h1>
       <h1 className='subtitle'>SHOP NOW</h1>
     </div>
   </div>
