9a121f1e932148b163291ab615866a3da78f7c8b
unknown <zoka_cs_pro@hotmail.com>
2020-09-12 12:39:14 +0200

    Created pages and components folder, added the homepage into pages folder

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-import HomePage from "./homepage.component";
+import HomePage from '../src/pages/homepage.component';
 
 function App() {
   return (
     <div>
-      {" "}
+      {' '}
       <HomePage />
     </div>
   );
