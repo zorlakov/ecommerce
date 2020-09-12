3e02220dcf7deb62e299e4f1e5e561a8893b222f
unknown <zoka_cs_pro@hotmail.com>
2020-09-13 00:42:51 +0200

    Added a custom button & connected it to the sign in component, properly styled the sign in component

--- ecommerce/src/components/sign-in/sign-in.component.jsx
+++ ecommerce/src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-botton/custom-button.component';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -30,24 +33,25 @@ class SignIn extends React.Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name='email'
             type='email'
             value={this.state.email}
-            onChange={this.handleChange}
+            label='email'
+            handleChange={this.handleChange}
             required
           />
-          <label>Email</label>
-          <input
+
+          <FormInput
             name='password'
             type='password'
             value={this.state.password}
-            onChange={this.handleChange}
+            label='password'
+            handleChange={this.handleChange}
             required
           />
-          <label>Password</label>
 
-          <input type='submit' value='Submit form' />
+          <CustomButton type='submit'> Sign In </CustomButton>
         </form>
       </div>
     );
