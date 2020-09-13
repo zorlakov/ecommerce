38d51a7d812fd4a9215fa8ddc78e3c1ab6fb5531
unknown <zoka_cs_pro@hotmail.com>
2020-09-13 12:29:18 +0200

    Added styling for sign in page buttons

--- ecommerce/src/components/sign-in/sign-in.component.jsx
+++ ecommerce/src/components/sign-in/sign-in.component.jsx
@@ -51,12 +51,13 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
             required
           />
-
-          <CustomButton type='submit'> Sign In </CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-            {' '}
-            Sign in with Google{' '}
-          </CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign In </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              {' '}
+              Sign in with Google{' '}
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
