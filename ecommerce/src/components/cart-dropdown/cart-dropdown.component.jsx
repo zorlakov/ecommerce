c820b36736b09e189f2e3325687714d3c070c93b
unknown <zoka_cs_pro@hotmail.com>
2020-09-15 15:49:16 +0200

    Implemented and styled the checkout page to show a list of items in the cart

--- ecommerce/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ ecommerce/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import CustomButton from '../custom-botton/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.length ? (
@@ -16,7 +17,12 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className='empty-message'>Your cart is empty</span>
       )}
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>
+    <CustomButton
+      onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden);
+      }}
+    >
       Go to checkout
     </CustomButton>
   </div>
