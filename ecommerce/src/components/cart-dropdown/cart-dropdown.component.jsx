5fb719dc34bff7da27a8ed0bd335c69dba29aedd
unknown <zoka_cs_pro@hotmail.com>
2020-09-15 15:25:47 +0200

    Created checkoutpage component, added routing to the checkout page

--- ecommerce/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ ecommerce/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import CustomButton from '../custom-botton/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
-      {cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className='empty-message'>Your cart is empty</span>
+      )}
     </div>
-    <CustomButton>Go to checkout</CustomButton>
+    <CustomButton onClick={() => history.push('/checkout')}>
+      Go to checkout
+    </CustomButton>
   </div>
 );
 
@@ -20,4 +27,4 @@ const mapStateToProps = ({ cart: { cartItems } }) => ({
   // Alternative: take whole state, cartItems: selectCartItems(state) -- with selector
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
