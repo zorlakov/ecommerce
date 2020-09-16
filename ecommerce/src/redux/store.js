7959f26580bd17eabe9e2b71e92e6fb03d027f30
unknown <zoka_cs_pro@hotmail.com>
2020-09-17 00:44:15 +0200

    Implemented local storage with redux persist so when we refresh checkout page, all existing items are still there

--- ecommerce/src/redux/store.js
+++ ecommerce/src/redux/store.js
@@ -2,11 +2,15 @@ import { createStore, applyMiddleware } from 'redux';
 // Middleware is added so when actions are fired we can
 // catch & display them
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
+// With persist our browser can cache our store
 
 import rootReducer from './root-reducer';
 
 const middleWares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middleWares));
+export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
-export default store;
+export const persistor = persistStore(store);
+
+//export default { store, persistor };
